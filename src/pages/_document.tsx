45fa414e270030625aec07a5c4770169089aa5ef
Natanael Lima <taelima1997@gmail.com>
2021-03-30 16:52:12 -0300

    add font roboto

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,11 +6,13 @@ export default class MyDocument extends Document {
       <Html lang="pt">
         <Head>
           <meta charSet="utf-8" />
+
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap"
             rel="stylesheet"
           />
+
           <link rel="shortcut icon" href="/favicon.png" />
         </Head>
         <body>
