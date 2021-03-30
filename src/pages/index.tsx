b1385e17665030be966cf00167d2af988ebc58d4
Natanael Lima <taelima1997@gmail.com>
2021-03-30 17:04:24 -0300

    add header to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,6 @@
+import { Header } from '../components/Header';
 import styles from '../styles/Home.module.scss';
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <h1> ig news</h1>
-    </div>
-  );
+  return <Header />;
 }
