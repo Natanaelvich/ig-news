3ca15890fc0a3336b0e171bd7d2bcbf9c3378c2b
Natanael Lima <taelima1997@gmail.com>
2021-03-30 17:35:50 -0300

    create home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,60 @@
+import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
+// import { SubscribeButton } from '../components/SubscribeButton';
+// import { stripe } from '../services/stripe';
+
 import styles from '../styles/Home.module.scss';
 
-export default function Home() {
-  return <h1>HOME</h1>;
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
 }
+
+export default function Home({ product }: HomeProps) {
+  return (
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for 12 month</span>
+            {/* <span>for { product.amount } 12 month</span> */}
+          </p>
+          <SubscribeButton priceId="12" />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
+    </>
+  );
+}
+
+// export const getStaticProps: GetStaticProps = async () => {
+//   const price = await stripe.prices.retrieve('price_1IYJ7WCrN0slKqO6rQQZVvOz');
+
+//   const product = {
+//     priceId: price.id,
+//     amount: new Intl.NumberFormat('en-US', {
+//       style: 'currency',
+//       currency: 'USD',
+//     }).format(price.unit_amount / 100),
+//   }
+
+//   return {
+//     props: {
+//       product,
+//     },
+//     revalidate: 60 * 60 * 24, // 24 horas
+//   }
+// };
