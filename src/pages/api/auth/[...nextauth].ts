6e5e8e7666fe2bad0e7fce0e3180115f33b18bb7
Natanael Lima <taelima1997@gmail.com>
2021-04-07 22:32:21 -0300

    create session funtion

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -12,6 +12,40 @@ export default NextAuth({
       scope: 'read:user',
     }),
   ],
+  callbacks: {
+    async session(session) {
+      try {
+        const userActiveSubscription = await fauna.query(
+          q.Get(
+            q.Intersection([
+              q.Match(
+                q.Index('subscription_by_user_ref'),
+                q.Select(
+                  'ref',
+                  q.Get(
+                    q.Match(
+                      q.Index('user_by_email'),
+                      q.Casefold(session.user.email),
+                    ),
+                  ),
+                ),
+              ),
+              q.Match(q.Index('subscription_by_status'), 'active'),
+            ]),
+          ),
+        );
+
+        return {
+          ...session,
+          activeSubscription: userActiveSubscription,
+        };
+      } catch {
+        return {
+          ...session,
+          activeSubscription: null,
+        };
+      }
+    },
     async signIn(user, account, profile) {
       const { email } = user;
 
@@ -33,4 +67,5 @@ export default NextAuth({
         return false;
       }
     },
+  },
 });
